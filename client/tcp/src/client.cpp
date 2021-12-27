2f0329f7ea41c846e6863b9538664b56e92f8339
Alan Jian <alanjian85@outlook.com>
2021-12-27 21:45:40 +0800

    Fix EOF bug and add TCP client main.cpp

--- client/tcp/src/client.cpp
+++ client/tcp/src/client.cpp
@@ -22,7 +22,7 @@ void client::do_connect(asio::ip::tcp::resolver::results_type endpoints) {
 void client::do_read() {
     asio::async_read(socket_, asio::buffer(buffer_),
         [this](const asio::error_code& ec, std::size_t bytes_transferred) {
-            if (!ec) {
+            if (!ec || ec == asio::error::eof) {
                 std::cout.write(buffer_.data(), bytes_transferred); 
             }
         });
--- client/tcp/src/main.cpp
+++ client/tcp/src/main.cpp
@@ -1,3 +1,18 @@
-int main() {
+#include <iostream>
 
+#include "client.hpp"
+using namespace daytime::client::tcp;
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: daytime_client_tcp <host>\n";
+        return 1;
+    }
+
+    asio::io_context io_context;
+    
+    client c(io_context, argv[1]);
+
+    io_context.run();
+    return 0;
 }
