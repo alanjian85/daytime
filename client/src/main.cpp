383ef7eb0d4a749e5c4fa6c70c8f98ed2dc37692
Alan Jian <alanjian85@outlook.com>
2021-12-28 19:19:49 +0800

    Server error handling

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -27,7 +27,7 @@ int main(int argc, char* argv[]) {
             udp::client c;
             c.run(argv[1]);
         }
-    } catch (const std::exception& e) {
+    } catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
         return 0;
     }
--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,13 +1,21 @@
+#include <iostream>
+#include <stdexcept>
+
 #include "tcp/server.hpp"
 #include "udp/server.hpp"
 using namespace daytime::server;
 
 int main() {
-    asio::io_context io_context;
+    try {
+        asio::io_context io_context;
 
-    tcp::server tcp_server(io_context);
-    udp::server udp_server(io_context);
+        tcp::server tcp_server(io_context);
+        udp::server udp_server(io_context);
 
-    io_context.run();
+        io_context.run();
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
     return 0;
 }
